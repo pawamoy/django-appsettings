64552af6b4557dc793f5703ff727b36627120991
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-12-03 14:34:44 +0100

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-app-settings',
-    version='0.3.0',
+    version='0.5.0',
     license='ISC',
     description='Application settings helper for Django apps.',
     long_description='%s\n%s' % (
