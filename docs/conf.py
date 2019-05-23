1cf0580f13f0924b6039a7b65ed4dd3a004dda77
Timothée Mazzucotelli <timothee.mazzucotelli@pm.me>
2019-05-23 21:31:25 +0200

    Prepare version 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ project = u'Django AppSettings'
 year = '2017'
 author = u'Timothee Mazzucotelli'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'0.5.0'
+version = release = u'0.5.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
